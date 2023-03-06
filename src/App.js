c8e9d0910ebf4951d669e9ddf966cf022263f99b
sksglobant <sks_globant@discovery.com>
2023-03-06 17:06:48 -0300

    feat: add code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 // Add code to import the other components here under
 
@@ -15,17 +19,35 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {/* Add Budget component here under */}    
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>    
 
                         {/* Add Remaining component here under */}        
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
+                        {/* Add ExpenseTotal component here under */}   
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>  
+                    </div>
+                    <div className='row mt-3'>
+                        <h3 className='mt-3'>Allocations</h3>
+                        {/* Add ExpenseList component here under */}   
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>      
+                    </div>
+                    <div className='row mt-3'>
+                        <h3 className='mt-3'>Change Allocation</h3>
+                        {/* Add ExpenseItem component here under */}     
 
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
+                        {/* Add AllocationForm component here under */}    
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>    
 
                 </div>
             </div>
